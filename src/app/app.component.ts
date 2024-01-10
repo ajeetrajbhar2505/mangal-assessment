5df01894ba92e18260a012023726a7a6f62eb31e
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-01-10 20:15:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ export class AppComponent {
   activeImagePath = 'product-1.png'
   productImages = ['product-1.png','product-2.png','product-3.png','product-4.png']
   index = 0
+  toogle:boolean = false
+
+  tooglMenu(){
+    this.toogle = ! this.toogle
+  }
   setActiveImage(path:string,index:number){
     this.index = index
     this.activeImagePath = path
